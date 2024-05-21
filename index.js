c00d8f540e0c59a203701a7c0aa0a47299dc3d83
Pijus Bilinskas <pijus.bilins@gmail.com>
2024-05-21 13:56:15 +0300

    yes

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+.connect(process.env.MONGO_CONNECTION)
+.then(() => console.log("Connected to DB"))
+.catch((err) => {
+    console.log("ERR", err)
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("App started on port", process.env.PORT);
+  });
