79605db2ab07cdd15478753fe1a835dee232e906
Pijus Bilinskas <pijus.bilins@gmail.com>
2024-05-23 15:11:31 +0300

    removed authentification for some endpoints

--- src/routes/question.js
+++ src/routes/question.js
@@ -12,11 +12,11 @@ import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/questions", auth, GET_ALL_QUESTIONS)
+router.get("/questions", GET_ALL_QUESTIONS)
 router.post("/question", auth, INSERT_QUESTION)
 router.delete("/question/:id", auth, DELETE_QUESTION_BY_ID)
 
-router.get("/question/:id/answers", auth, GET_ANSWERS_FOR_QUESTION_BY_ID)
+router.get("/question/:id/answers", GET_ANSWERS_FOR_QUESTION_BY_ID)
 router.post("/question/:id/answers", auth, POST_ANSWER_FOR_QUESTION_BY_ID)
 router.delete("/answer/:id", auth, DELETE_ANSWER)
 
